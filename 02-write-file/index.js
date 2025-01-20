624e4cf3bcf953ca1c2848a42e970bece954c9d2
Maksim <maksim.vasileuskij@gmail.com>
2025-01-20 22:59:36 +0300

    feat: add addition script exit, with key phrase, refactor exit messages.

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 
 const { stdin, stdout } = process;
 
+stdin.setEncoding('utf-8');
+
 let str = '';
 
 const path = require('path');
@@ -14,18 +16,26 @@ console.log(`
 ===========================================
 `);
 
-stdin.on('data', (stream) => {
-  output.write(stream);
-  str += stream;
+const exitProgram = () => {
+  console.log('\n=====================================');
+  console.log('You have finished, this is your file!');
+  console.log('=====================================');
+  console.log(str);
+  process.exit(0); // Завершение программы
+};
+
+stdin.on('data', (data) => {
+  const input = data.trim();
+  if (input.toLowerCase() === 'exit') {
+    exitProgram();
+  }
+  output.write(data);
+  str += data;
 });
 
 const output = fs.createWriteStream(directoryPath, {
   encoding: 'utf-8',
 });
 process.on('SIGINT', () => {
-  stdout.write(
-    '\n===========================================\nYou have finished, this is your result:\n===========================================\n',
-  );
-  stdout.write(str);
-  process.exit();
+  exitProgram();
 });
