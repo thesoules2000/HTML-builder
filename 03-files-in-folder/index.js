e73c33a5551a15493339929d4e22dc76ef3eea39
Maksim <maksim.vasileuskij@gmail.com>
2025-01-20 22:34:53 +0300

    refactor: replace standard output (console.log()) with console.table() for better readability

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -6,15 +6,22 @@ const directoryPath = path.join(__dirname, 'secret-folder');
 async function readFiles() {
   try {
     const files = await fs.readdir(directoryPath, { withFileTypes: true });
+    const filesData = [];
 
     for (const file of files) {
       if (file.isFile()) {
         const filePath = path.join(directoryPath, file.name);
         const type = path.extname(file.name).slice(1);
         const stats = await fs.stat(filePath);
-        console.log(`${file.name} - ${type} - ${stats.size} bytes`);
+
+        filesData.push({
+          Name: file.name,
+          Type: type,
+          Size: `${stats.size} bytes`,
+        });
       }
     }
+    console.table(filesData);
   } catch (err) {
     console.log(err);
   }
